3a72f459a3d875b4bc34d08f70a251c43bb8e2c6
Leodwig <leo.jamsa@hotmail.com>
2015-06-08 02:33:39 +0300

    Yksiköitä kartalla!

---
+++ Projekti56/Assets/Skriptit/UnitController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+
+	public GameObject[] units;
+	public Vector2[] coordinates;
+	
+	void Start () {
+	
+	for (int i = 0; i < units.Length; i++) {
+			GameObject unit = units[i];
+			
+			// Instantioi unitin koordinaatteihin
+			GameObject instantiatedUnit = (GameObject)Instantiate(unit, new Vector3(0,0,0), Quaternion.identity);
+			
+			// Groundcontrollerin taulukosta haetaan tile, johon unit ollaan laittamassa
+			GameObject tile = GroundController.instance.tilesAsArray[(int)coordinates[i].x,(int)coordinates[i].y];
+			
+			instantiatedUnit.transform.parent = tile.transform;
+			instantiatedUnit.transform.position = tile.transform.position + new Vector3(0,0,0.1f);
+		}
+}
+}
