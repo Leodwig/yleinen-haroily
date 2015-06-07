948dd1eee6be7f4899bb5d2e619c3a71a21dad45
Ville Talonpoika <talonvi@gmail.com>
2015-06-08 01:13:27 +0300

    Lisätty kenttäpohja

---
+++ Projekti56/Assets/Skriptit/CameraMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+	public float speed = 1;
+	
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+		}
+	}
+
+}
---
+++ Projekti56/Assets/Skriptit/GroundController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundController : MonoBehaviour {
+
+	public GameObject basicGround;
+	
+	public int width = 10;
+	public int height = 10;
+	
+	public float tileWidth = 0.5f;
+	public float tileHeight = 0.5f;
+	
+	void Start () {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				GameObject tile = (GameObject)Instantiate(basicGround, new Vector3(i * tileWidth, j * tileHeight, 1), Quaternion.identity);
+				tile.transform.parent = transform;
+			}
+		}
+		transform.position = new Vector3(-(width*tileWidth)/2,-(height*tileHeight)/2,1);
+	}
+	
+	void Update () {
+	
+	}
+}
