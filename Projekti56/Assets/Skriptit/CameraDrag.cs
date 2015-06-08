9b83c332dd99b9beff82b9903e55be9c9a4912ce
Leodwig <leo.jamsa@hotmail.com>
2015-06-09 02:17:06 +0300

    Mielenkiintoinen kameraliikutus!

---
+++ Projekti56/Assets/Skriptit/CameraDrag.cs
@@ -0,0 +1,26 @@
+﻿    using UnityEngine;
+     
+    public class CameraDrag : MonoBehaviour
+    {
+        public float dragSpeed = 2;
+        private Vector3 dragOrigin;
+     
+     
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragOrigin = Input.mousePosition;
+                return;
+            }
+     
+            if (!Input.GetMouseButton(0)) return;
+     
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+     
+            transform.Translate(move, Space.World);  
+        }
+     
+     
+    }
