1a033385c9520e5102a6ecda83f6f4eed1f0c3d2
Leodwig <leo.jamsa@hotmail.com>
2015-06-09 01:09:15 +0300

    Koodia vol.3!

---
+++ Projekti56/Assets/Skriptit/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if (Physics.Raycast(ray, out hit)) {
+				// Tähän settiä
+				Debug.Log(hit.transform.name);
+			
+			}
+		}
+	}
+}
