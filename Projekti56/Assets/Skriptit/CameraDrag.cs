6ede9e2861cc17bd703cad78132814f451889721
Leodwig <leo.jamsa@hotmail.com>
2015-06-09 17:05:11 +0300

    Pieni parannus Framen spriteen!

--- Projekti56/Assets/Skriptit/CameraDrag.cs
+++ Projekti56/Assets/Skriptit/CameraDrag.cs
@@ -2,41 +2,25 @@
      
     public class CameraDrag : MonoBehaviour
     {
-        //public float dragSpeed = 2;
-        //private Vector3 dragOrigin;
+        public float dragSpeed = 2;
+        private Vector3 dragOrigin;
      
      
-        //void Update()
-        //{
-          //  if (Input.GetMouseButtonDown(0))
-            //{
-              //  dragOrigin = Input.mousePosition;
-                //return;
-            //}
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragOrigin = Input.mousePosition;
+                return;
+            }
      
-    //        if (!Input.GetMouseButton(0)) return;
-     //
-       //     Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-         //   Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+            if (!Input.GetMouseButton(0)) return;
      
-           // transform.Translate(move, Space.World);  
-        //}
-                  if(Input.GetMouseButtonDown(0))
-             {
-                 bDragging = true;
-                 oldPos = transform.position;
-                 panOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);                    //Get the ScreenVector the mouse clicked
-             }
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
      
-             if(Input.GetMouseButton(0))
-             {
-                 Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - panOrigin;    //Get the difference between where the mouse clicked and where it moved
-                 transform.position = oldPos + -pos * panSpeed;                                         //Move the position of the camera to simulate a drag, speed * 10 for screen to worldspace conversion
-             }
+            transform.Translate(move, Space.World);  
+        }
      
-             if(Input.GetMouseButtonUp(0))
-             {
-                 bDragging = false;
-             }
      
     }
