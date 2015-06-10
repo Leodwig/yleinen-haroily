ed7135ea7b3c9578869694e8d37cd395fe6c0a61
Leodwig <leo.jamsa@hotmail.com>
2015-06-11 01:59:03 +0300

    Koodia vol.8!
    Naapureidenkorostus "toimii": mukana hauskoja bugeja!

--- Projekti56/Assets/Skriptit/Tile.cs
+++ Projekti56/Assets/Skriptit/Tile.cs
@@ -7,6 +7,9 @@ public class Tile : MonoBehaviour {
 	
 	// Tarvittava movement, että yksikkö voi siirtyä tileen
 	public int movementCost;
+	
+	// Kertoo, voiko yksikkö siirtyä tileen
+	public bool moveable = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +18,31 @@ public class Tile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (clicked == true) {
+		if (clicked) {
 			transform.Find("Frame").gameObject.SetActive(true);
+			HighlightNeighbours();
 		}
 		else {
 			transform.Find("Frame").gameObject.SetActive(false);
 		}
+		
+		
+	}
+
+	private void HighlightNeighbours () {
+		// Naapureiden highlight
+		GameObject[] tileObjects = GroundController.instance.GetNeighbours(gameObject);
+		Tile[] tiles = new Tile[tileObjects.Length];
+		for (int i = 0; i < tileObjects.Length; i++) {
+			tiles[i] = tileObjects[i].GetComponent<Tile> ();
+		}
+		foreach (Tile tile in tiles) {
+			tile.HighlightThis();
+		}
+	}
+	
+	public void HighlightThis () {	// Highlightausfunktio
+		transform.Find("Frame").gameObject.SetActive(true);
+		transform.Find("Frame").gameObject.GetComponent<Animator>().enabled = false;
 	}
 }
