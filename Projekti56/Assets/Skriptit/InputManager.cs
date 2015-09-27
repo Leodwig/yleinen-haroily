09936497c7bf9f304ff3f3568356ff7d8424cb45
Ville Talonpoika <talonvi@gmail.com>
2015-09-27 23:45:17 +0300

    Fixed tile selection bug

--- Projekti56/Assets/Skriptit/InputManager.cs
+++ Projekti56/Assets/Skriptit/InputManager.cs
@@ -3,33 +3,28 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour {
 
-	private GameObject selected;
+    private GameObject selected;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0)){
-			Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			
-			if (Physics.Raycast(ray, out hit)) {
-				// Tähän settiä
-				Debug.Log(hit.transform.name);
-				
-				if (hit.transform.GetComponent<Tile>()) {
-					if (selected != null) {
-						if (selected.GetComponent<Tile>()) {
-							selected.GetComponent<Tile>().clicked = false;
-						}
-					}
-					selected = hit.transform.gameObject;
-					selected.GetComponent<Tile>().clicked = true;
-				}
-			}
-		}
-	}
+    private GroundController gc;
+
+    void Start() {
+        gc = GroundController.instance;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.GetComponent<Tile>()) {
+                    foreach (GameObject obj in gc.tilesAsArray) {
+                        obj.transform.GetComponent<Tile>().Unclick();
+                    }
+                    selected = hit.transform.gameObject;
+                    selected.GetComponent<Tile>().Click();
+                }
+            }
+        }
+    }
 }
--- Projekti56/Assets/Skriptit/Tile.cs
+++ Projekti56/Assets/Skriptit/Tile.cs
@@ -3,47 +3,65 @@ using System.Collections;
 
 public class Tile : MonoBehaviour {
 	
-	public bool clicked = false;
-	
+	private bool clicked = false;
+    private bool selected = false;
+
 	// Tarvittava movement, että yksikkö voi siirtyä tileen
 	public int movementCost;
 	
 	// Kertoo, voiko yksikkö siirtyä tileen
 	public bool moveable = false;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (clicked) {
-			transform.Find("Frame").gameObject.SetActive(true);
-			HighlightNeighbours();
+            if (!selected) {
+                SelectThis();
+                HighlightNeighbours();
+                selected = true;
+            }
 		}
-		else {
-			transform.Find("Frame").gameObject.SetActive(false);
-		}
-		
-		
 	}
 
 	private void HighlightNeighbours () {
 		// Naapureiden highlight
 		GameObject[] tileObjects = GroundController.instance.GetNeighbours(gameObject);
 		Tile[] tiles = new Tile[tileObjects.Length];
-		for (int i = 0; i < tileObjects.Length; i++) {
-			tiles[i] = tileObjects[i].GetComponent<Tile> ();
-		}
-		Debug.Log(tiles.Length);
-		foreach (Tile tile in tiles) {
-			tile.HighlightThis();
-		}
-	}
-	
-	public void HighlightThis () {	// Highlightausfunktio
+        for (int i = 0; i < tileObjects.Length; i++) {
+            tileObjects[i].GetComponent<Tile>().HighlightThis();
+        }
+    }
+
+    private void UnhighlightNeighbours() {
+        GameObject[] tileObjects = GroundController.instance.GetNeighbours(gameObject);
+        Tile[] tiles = new Tile[tileObjects.Length];
+        for (int i = 0; i < tileObjects.Length; i++) {
+            tileObjects[i].GetComponent<Tile>().UnselectThis();
+        }
+    }
+
+    private void SelectThis() {
+        transform.Find("Frame").gameObject.SetActive(true);
+        transform.Find("Frame").gameObject.GetComponent<Animator>().enabled = true;
+    }
+
+    private void HighlightThis () {	// Highlightausfunktio
 		transform.Find("Frame").gameObject.SetActive(true);
 		transform.Find("Frame").gameObject.GetComponent<Animator>().enabled = false;
 	}
+
+    private void UnselectThis() {
+        transform.Find("Frame").gameObject.SetActive(false);
+    }
+
+    public void Click() {
+        clicked = true;
+    }
+
+    public void Unclick() {
+        clicked = false;
+        selected = false;
+        UnselectThis();
+        UnhighlightNeighbours();
+    }
 }
